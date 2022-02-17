2396c9af3d72db7f27f810355d20c581e1a52efc
Anders Aaen Springborg <aaspringborg@gmail.com>
2022-02-17 17:47:01 +0100

    add battery pct to view

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Battery from 'expo-battery';
+
 
 export default function App() {
+  const [battery, setBattery] = useState<number>(0);
+
+  useEffect(() => {
+    Battery.getBatteryLevelAsync().then(b => setBattery(b));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Battery {(battery * 100).toFixed(0)}%</Text>
+      <StatusBar style="auto" backgroundColor={"black"}/>
     </View>
   );
 }
