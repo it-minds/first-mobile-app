55d73abe74dca143df803af1d4fa50c43b0de432
Anders Aaen Springborg <aaspringborg@gmail.com>
2022-02-18 18:52:16 +0100

    Low powered mode

--- App.tsx
+++ App.tsx
@@ -14,10 +14,38 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Battery {(battery * 100).toFixed(0)}%</Text>
+      <LowPoweredMode/>
       <StatusBar style="auto" backgroundColor={"black"}/>
     </View>
   );
 }
+
+
+const LowPoweredMode = () => {
+  const [lowPoweredState, setLowPoweredState] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    Battery.getPowerStateAsync().then(be => setLowPoweredState(be.lowPowerMode));
+
+    const batteryModeSubscriber = Battery.addLowPowerModeListener(b => setLowPoweredState(b.lowPowerMode))
+    return () => batteryModeSubscriber.remove();
+  }, [])
+
+  if (lowPoweredState === null) return <Text>Getting info from your phone...</Text>;
+  return (
+    <Text>Battery state: {lowPoweredState ?
+      "i am a static text replacement" :
+      "I AM A POWER HUNGRY ANIMATION"}
+  </Text>
+  )
+
+}
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
